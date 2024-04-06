949440602edfa43e0d2e1037bca98ad86aaa3a9d
AlexEdich <alexedich23@gmail.com>
2024-04-06 19:08:38 +0200

    Added func createSampleCode and to context menu

--- out/extension.js
+++ out/extension.js
@@ -32,7 +32,7 @@ const vscode = __importStar(require("vscode"));
 function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "mctg" is now active!');
+    //console.log('Congratulations, your extension "mctg" is now active!');
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
@@ -42,6 +42,30 @@ function activate(context) {
         vscode.window.showInformationMessage('Hello World from MultiConditionTestGenerator!');
     });
     context.subscriptions.push(disposable);
+    let createSampleCode = vscode.commands.registerCommand('mf.createSampleCode', function () {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            editor.edit(editBuilder => {
+                //write at the end of the document
+                const cursorPosition = editor.document.lineAt(editor.document.lineCount - 1).range.end;
+                //write code
+                editBuilder.insert(cursorPosition, "\n\n//This is for debugging\n" +
+                    "public int bonus(int score, int alt) {\n" +
+                    "int ergebnis = 0;\n" +
+                    "if (score > 2600) {\n" +
+                    "\tergebnis = 20;\n" +
+                    "} else {\n" +
+                    "\tergebnis = 5;\n" +
+                    "}\n" +
+                    "if (alt > 42 && score > 900) {\n" +
+                    "\tergebnis += 10;\n" +
+                    "}\n" +
+                    "return ergebnis;\n" +
+                    "}\n");
+            });
+        }
+    });
+    context.subscriptions.push(createSampleCode);
 }
 exports.activate = activate;
 // This method is called when your extension is deactivated
--- src/extension.ts
+++ src/extension.ts
@@ -8,7 +8,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "mctg" is now active!');
+	//console.log('Congratulations, your extension "mctg" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -18,8 +18,32 @@ export function activate(context: vscode.ExtensionContext) {
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from MultiConditionTestGenerator!');
 	});
-
 	context.subscriptions.push(disposable);
+
+	let createSampleCode = vscode.commands.registerCommand('mf.createSampleCode', function () {
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			editor.edit(editBuilder => { 
+				//write at the end of the document
+				const cursorPosition = editor.document.lineAt(editor.document.lineCount - 1).range.end;
+				//write code
+				editBuilder.insert(cursorPosition, "\n\n//This is for debugging\n" +
+													"public int bonus(int score, int alt) {\n" +
+													"int ergebnis = 0;\n" +
+													"if (score > 2600) {\n" +
+													"\tergebnis = 20;\n" +
+													"} else {\n" +
+													"\tergebnis = 5;\n" +
+													"}\n" +
+													"if (alt > 42 && score > 900) {\n" +
+													"\tergebnis += 10;\n" +
+													"}\n" +
+													"return ergebnis;\n" +
+													"}\n");
+			});
+		}
+	});
+	context.subscriptions.push(createSampleCode);
 }
 
 // This method is called when your extension is deactivated
